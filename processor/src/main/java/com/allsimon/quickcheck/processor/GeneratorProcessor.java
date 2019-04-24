47701e43f0b578a7a86ab761e028f3ac4d6867e2
Simon A <simon.allegraud.partner@gmail.com>
2019-04-24 15:37:35 +0200

    cleanup: remove validation

--- processor/src/main/java/com/allsimon/quickcheck/processor/GeneratorProcessor.java
+++ processor/src/main/java/com/allsimon/quickcheck/processor/GeneratorProcessor.java
@@ -5,13 +5,11 @@ import static java.util.stream.Collectors.toSet;
 
 import com.allsimon.quickcheck.Generator;
 import com.allsimon.quickcheck.Generator.List;
-import com.allsimon.quickcheck.processor.jsr380.ValidGeneratorBuilder;
 import com.google.auto.service.AutoService;
 import com.squareup.javapoet.JavaFile;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import javax.annotation.processing.AbstractProcessor;
@@ -20,12 +18,9 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.Diagnostic;
-import javax.tools.Diagnostic.Kind;
 
 @AutoService(Processor.class)
 public class GeneratorProcessor extends AbstractProcessor {
@@ -54,25 +49,9 @@ public class GeneratorProcessor extends AbstractProcessor {
 
   private void handle(Generator annotation, Element element) {
     TypeMirror classToGenerate = Utils.getMirroredThing(t -> annotation.value());
-    Element elementToGenerate = processingEnv.getTypeUtils().asElement(classToGenerate);
 
     Set<JavaFile.Builder> javaFiles = new HashSet<>();
     javaFiles.add(new GeneratorBuilder(get(classToGenerate)).javaFileBuilder());
-
-    if (Utils.hasFieldAnnotatedWithJSR380(elementToGenerate)) {
-      Optional<java.util.List<
-          ? extends VariableElement>> constructorArgs = Utils.findBiggestArgsConstructor(elementToGenerate)
-          .map(t -> (ExecutableElement) t)
-          .map(ExecutableElement::getParameters);
-      if (!constructorArgs.isPresent()) {
-        processingEnv.getMessager()
-            .printMessage(Kind.ERROR, "No public constructor found for: " + element.getSimpleName());
-      } else {
-        javaFiles.add(new ValidGeneratorBuilder(get(classToGenerate),
-            constructorArgs.get(),
-            Utils.getJSR380Annotations(elementToGenerate)).javaFileBuilder());
-      }
-    }
 
     javaFiles.forEach(fileBuilder -> {
       try {
